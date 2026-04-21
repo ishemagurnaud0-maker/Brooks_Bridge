34e0fd30b32e7bd014ca5b06f3f3bb2db808459f
Gurnaud <ishemagurnaud0@gmail.com>
2026-04-21 23:03:05 +0100

    [refactor]:new file structure

---
+++ brooks-bridge-api/src/modules/departments/departments.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { CreateDepartmentDto } from './dto/create-department.dto';
+import { UpdateDepartmentDto } from './dto/update-department.dto';
+
+@Injectable()
+export class DepartmentsService {
+ 
+}
