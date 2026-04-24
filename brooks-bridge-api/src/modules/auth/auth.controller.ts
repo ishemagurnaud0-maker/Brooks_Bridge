da3b1927324413743e979579fade4147491d6b57
Gurnaud <ishemagurnaud0@gmail.com>
2026-04-24 07:30:42 +0100

    [feat]:OTP email validation.

--- brooks-bridge-api/src/modules/auth/auth.controller.ts
+++ brooks-bridge-api/src/modules/auth/auth.controller.ts
@@ -1,9 +1,33 @@
-import { Controller} from '@nestjs/common';
+import { Controller,UseGuards} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterCompanyDto,RegisterOwnerDto,LoginDto } from './dto'
+import { Public } from '../../common/decorators/public.decorator'
+import { CurrentUser } from '../../common/decorators/current-user-decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { Post,Body } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
+  @Post('register/owner')
+  async registerOwner(@Body() user:RegisterOwnerDto){
+    return this.authService.registerOwner(user);
+  }
+
+
+  @UseGuards(JwtAuthGuard)
+  @Post('register/company')
+   async registerCompany(@CurrentUser() user,@Body() company:RegisterCompanyDto){
+    return this.authService.RegisterCompany(user.id,company);
+  }
+
+  @Public()
+  @UseGuards()
+  @Post('login')
+   async login(@Body() loginCreds:LoginDto ) {
+    return this.authService.login(loginCreds);
+   }
   
 }
