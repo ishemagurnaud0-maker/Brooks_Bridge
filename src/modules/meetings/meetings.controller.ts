ea7e6a35e6e0c22f663bbf9a40ed7e12aa5eb8da
Gurnaud <ishemagurnaud0@gmail.com>
2026-04-25 22:20:15 +0100

    [refactor]:the meetings module.

--- src/modules/meetings/meetings.controller.ts
+++ src/modules/meetings/meetings.controller.ts
@@ -1,10 +1,37 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { MeetingsService } from './meetings.service';
 import { CreateMeetingDto } from './dto/create-meeting.dto';
+import { UpdateMeetingDto } from './dto/update-meeting.dto';
+import { CurrentUser } from '../../common/decorators/current-user-decorator'
 
 
 @Controller('meetings')
 export class MeetingsController {
   constructor(private readonly meetingsService: MeetingsService) {}
 
+  @Post()
+  create(@CurrentUser() user,@Body() dto:CreateMeetingDto){
+    return this.meetingsService.createMeeting(user.companyId,user.id,dto);
+  }
+
+  @Get()
+  getAll(@CurrentUser() user){
+    return this.meetingsService.findAllMeetings(user.companyId,user.id);
+  }
+
+  @Get(':id')
+  getOne(@CurrentUser() user, @Param('meetingId') meetingId:string) {
+    return this.meetingsService.findOneMeeting(user.companyId,user.id,meetingId);
+  }
+
+  @Patch(':id') 
+  update(@CurrentUser() user,@Param('meetingId') meetingId:string,@Body() dto:UpdateMeetingDto) {
+    return this.meetingsService.updateMeeting(user.companyId,meetingId,dto);
+  }
+
+  @Delete(':id')
+  delete(@CurrentUser() user,@Param('meetingId') meetingId:string){
+    return this.meetingsService.deleteMeeting(user.companyId,meetingId);
+  }
+  
 }
--- src/modules/meetings/meetings.module.ts
+++ src/modules/meetings/meetings.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MeetingsService } from './meetings.service';
 import { MeetingsController } from './meetings.controller';
+import { ChatsModule } from '../chats/chats.module';
 
 @Module({
+  imports:[ChatsModule],
   controllers: [MeetingsController],
   providers: [MeetingsService],
 })
