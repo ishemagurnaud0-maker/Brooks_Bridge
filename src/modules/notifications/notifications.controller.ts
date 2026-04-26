4ce05babf8af7a6ceb523600397780152565a702
Gurnaud <ishemagurnaud0@gmail.com>
2026-04-26 10:19:14 +0100

    [refactor]:notif.contriller + module done

--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param} from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
+import { CurrentUser } from '../../common/decorators/current-user-decorator'
 
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
+
+  @Get()
+  findAll(@CurrentUser() user) {
+   return this.notificationsService.getMyNotifications(user.id,);
+  }
+
+  @Patch(':id')
+  markAsRead(@CurrentUser() user,@Param('notificationId') notificationId:string ){
+    return this.notificationsService.markAsRead(user.id,notificationId); 
+  }
+
+  @Patch()
+ markAllAsRead(@CurrentUser() user){
+  return this.notificationsService.markAsAllRead(user.id);
+ }
 
  
 }
