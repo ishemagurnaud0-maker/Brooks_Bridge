b12e89d746c7f01e3c3e6fd3b8a583799ce5984c
Gurnaud <ishemagurnaud0@gmail.com>
2026-04-26 11:04:40 +0100

    [feat]:added the notification services to the module providers

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { PaymentWebhook } from './payments.webhook'
+import { NotificationsGateway } from '../notifications/notifications.gateway';
+import { NotificationsService } from '../notifications/notifications.service'
+
 @Module({
   controllers: [PaymentsController],
-  providers: [PaymentsService,PaymentWebhook],
+  providers: [PaymentsService,PaymentWebhook,NotificationsService,NotificationsGateway],
 })
 export class PaymentsModule {}
