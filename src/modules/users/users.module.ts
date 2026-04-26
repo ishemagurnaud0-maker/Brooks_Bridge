c2e6cfeec832300aaf3230a3595a8b082e7bf000
Gurnaud <ishemagurnaud0@gmail.com>
2026-04-26 11:10:50 +0100

    [update]:added the notification service to the module providers

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { NotificationsGateway } from '../notifications/notifications.gateway';
+import { NotificationsService } from '../notifications/notifications.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    { provide: UsersService, useClass: UsersService },
+    { provide: NotificationsService, useClass: NotificationsService },
+    { provide: NotificationsGateway, useClass: NotificationsGateway },
+  ],
 })
 export class UsersModule {}
